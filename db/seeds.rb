cb9d8b325b50d93e5ce0899355962bae26b4d7db
BlinkVonDoom <blynk.rankin@gmail.com>
2018-11-24 22:45:30 -0700

    create seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do |blog|
+  Blog.create!(
+    title: "blog post #{blog}",
+    body: "Stuff here"
+  )
+end
+
+puts "10 blogs created"
+
+5.times do |skill|
+  Skill.create(
+    title: "Demo #{skill}",
+    percentUtilized: 15
+  )
+end
+
+puts "5 skills created"
+
+9.times do |portfolio|
+  Portfolio.create(
+      title: "protfolio title #{portfolio}",
+      subTitle: "new service",
+      body: "body text",
+      mainImage: "https://via.placeholder.com/600x400",
+      thumbImage: "https://via.placeholder.com/300x200"
+  )
+end
+
+puts "9 portfolios created"
