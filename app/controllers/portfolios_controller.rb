943f15f13b26133471148ab0e29d122fbf73b3d4
BlinkVonDoom <blynk.rankin@gmail.com>
2018-11-25 09:33:17 -0700

    add new portfolio routes

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -2,4 +2,24 @@ class PortfoliosController < ApplicationController
   def index
     @portfolioItems = Portfolio.all
   end
+
+  def new
+    @portfolioItem = Portfolio.new
+  end
+
+  def create
+    @portfolioItem = Portfolio.new(params.require(:portfolio).permit(:title, :subTitle ,:body))
+
+    respond_to do |format|
+      if @portfolioItem.save
+        format.html {
+          redirect_to portfolios_path, notice: "Your portfolio was created"
+        }
+      else
+        format.html{ render :new }
+      end
+    end
+  end
+
+
 end
